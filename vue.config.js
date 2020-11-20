d287fd5f139c10ba4caad331bef743599843859e
zhufue <499984532@qq.com>
2020-11-21 03:34:04 +0800

    别名处理

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports ={
+    configureWebpack:{
+resolve:{
+alias:{
+    //@表示src
+    'asserts':'@/asserts',
+    'common':'@/common',
+    'components':'@/components',
+    'network':'@/network',
+    'views':'@/views',
+}
+}
+}
+}
