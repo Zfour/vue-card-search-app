1aa1b37bba5688be7e3b7ae88149b85c07e26ee1
zhufue <499984532@qq.com>
2020-12-03 05:40:32 +0800

    接入了wp api

--- dist/js/chunk-2d21f83c.44c96380.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d21f83c"],{d9c4:function(e,n,a){"use strict";a.r(n);var c=function(){var e=this,n=e.$createElement,a=e._self._c||n;return a("h1",[e._v("game")])},t=[],u={name:"Game"},r=u,s=a("2877"),l=Object(s["a"])(r,c,t,!1,null,"2a0f4952",null);n["default"]=l.exports}}]);
-//# sourceMappingURL=chunk-2d21f83c.44c96380.js.map
